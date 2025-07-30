be11b2adbe81bd56ed0f5f73d15048eb74163e5e
pauuvl <pvelasquel@eafit.edu.co>
2025-07-30 11:47:40 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Movie
 
 def home(request):
-
-    return render(request, 'home.html',{'name':'Paulina Velasquez L'})
-
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {
+        'searchTerm': searchTerm,
+        'movies': movies
+    })
 def about(request):
     return render(request, 'about.html')
-
 
 # Create your views here.
